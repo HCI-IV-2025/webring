b3adf5820187858624baf2aaa9b3aaa49daa0b98
karlgzafiris <georgekzafiris@gmail.com>
2025-10-16 20:32:36 +0300

    member add: Karl G Zafiris

--- js/index.js
+++ js/index.js
@@ -33,7 +33,12 @@ const members = [
     name: 'Emmanouela Vagianou',
     icon: 'EV',
   },
-
+  {
+    id: 'karlgzafiris',
+    url: 'https://karlgzafiris.github.io/',
+    name: 'Karl Zafiris',
+    icon: 'kgz'
+  }
   // Προσθέστε νέα μέλη εδώ:
   // { id: 'maria', url: 'https://maria.github.io/cv', name: 'Μαρία Κ.', icon: 'Μ' }
 ];
